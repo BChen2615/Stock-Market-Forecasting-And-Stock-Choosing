742a7e4f91b4b4e767b553d2321d3c9403fd4c5a
Branden Chen <bc2615@berkeley.edu>
2025-12-30 22:53:58 -0800

    Initialize the DataProcess.py

--- src/DataProcess.py
+++ src/DataProcess.py
@@ -1,6 +1,7 @@
 # This is a python file for data processing
 import pandas as pd
 import numpy as np
+import datetime as dt
 
 def average_price(interval, data):
     """
@@ -18,3 +19,33 @@ def average_price(interval, data):
     return_data[f"MA_{interval}"] = return_data.groupby("Stock_ID")["Close"].transform(lambda x: x.rolling(interval).mean())
 
     return return_data
+
+def filter_for_date(df, start_date= dt.date(2020, 12, 24), end_date= dt.date.today()):
+    """
+    This is a function to filter the data based on the start date and end date
+
+    :param df: (pd.dataframe) Should be a dataframe of a database with all the stocks and historical data
+    :param start_date: (date) default is the date of 2020-12-24
+    :param end_date: (date) default is the current date (Note that the current date may not include in the database
+    :return: (pd.dataframe) filtered dataframe
+    """
+    filtered_data = df[
+        (df["Date"] >= pd.to_datetime(start_date)) &
+        (df["Date"] <= pd.to_datetime(end_date))
+    ]
+    return filtered_data
+
+def prediction_data_processing(df, pred_date):
+    """
+    This is the function to process the data from database to the features for prediction
+
+    :param df: (pd.dataframe) The dataframe that comes from the database
+    :param pred_date: (date) The date that we want to predict the stock price for
+    :return: (pd.dataframe) new dataframe that with each row is the one stock of taiwan stock market, and
+            each column is the feature of the stock
+    """
+
+    return_df = df.copy()
+    return_df = filter_for_date(return_df, pred_date - dt.timedelta(day= 365))
+
+
